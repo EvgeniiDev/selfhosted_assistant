8b9d51b8f1af92a2c88c943cf776c694d7ef2d11
Евгений <11@11.11>
2026-03-09 13:23:12 +0500

    feat: remove ModelRouter and OpenRouterProvider, simplify LLM inference structure

--- llm_inference/__init__.py
+++ llm_inference/__init__.py
@@ -3,9 +3,7 @@ LLM Inference Package
 Модуль для работы с различными LLM моделями
 """
 
-from llm_inference.model_router import ModelRouter
-from llm_inference.local_provider import LocalProvider  
-from llm_inference.openrouter_provider import OpenRouterProvider
+from llm_inference.local_provider import LocalProvider
 from llm_inference.privacy_detector import PrivacyDetector
 
-__all__ = ['ModelRouter', 'LocalProvider', 'OpenRouterProvider', 'PrivacyDetector']
+__all__ = ['LocalProvider', 'PrivacyDetector']
